Cosine Experiment with wrong gradient

app.py
@@ -32,7 +32,7 @@ def validate_args(args):
 
 
 def main(exp_name, slack, sampling_freq, grad_sampling_freq=None, flip_prob=None,
-         average=False, gamma=1):
+         average=False, gamma=1.0):
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset",
@@ -100,15 +100,15 @@ if __name__ == '__main__':
     FF = [16]
     NUM_ITERATIONS = 30
     slack = 0.10
-    G = np.round(10**np.linspace(0, -2, num=9), 2)
+    # G = np.round(10**np.linspace(0, -2, num=9), 2)
+    G = [10]
     for gamma in G:
         start = time.time()
-        main('adv/det_30_1000_g{}'.format(gamma),
+        main('adv/det_30_1000',
              slack=slack,
              sampling_freq=sampling_freq,
              grad_sampling_freq=None,
              flip_prob=None,
-             average=False,
-             gamma=gamma)
+             average=False)
         print(time.time() - start)
     pass

hopskip.py
@@ -2,7 +2,8 @@ import math
 import numpy as np
 import logging
 from adversarial import Adversarial
-
+from numpy import dot
+from numpy.linalg import norm
 
 class HopSkipJumpAttack:
     def __init__(self, model_interface, data_shape, initial_num_evals=100, max_num_evals=10000, distance="MSE",
@@ -33,8 +34,8 @@ class HopSkipJumpAttack:
         self.shape = data_shape
         self.d = np.prod(self.shape)
         if self.constraint == "l2":
-            # self.theta = self.gamma / (np.sqrt(self.d) * self.d)
-            self.theta = self.gamma / (np.sqrt(self.d))  # Based on CJ experiment
+            self.theta = self.gamma / (np.sqrt(self.d) * self.d)
+            # self.theta = self.gamma / (np.sqrt(self.d))  # Based on CJ experiment
         else:
             self.theta = self.gamma / (self.d * self.d)
 
@@ -67,7 +68,7 @@ class HopSkipJumpAttack:
             logging.info('Model Calls till now: %d' % self.model_interface.model_calls)
         original = a.unperturbed.astype(self.internal_dtype)
         perturbed = a.perturbed.astype(self.internal_dtype)
-        additional = {'iterations': list(), 'initial': perturbed, 'manifold': list()}
+        additional = {'iterations': list(), 'initial': perturbed, 'manifold': list(), 'cosine_details': list()}
 
         def decision_function(x, freq, average=False):
             outs = []
@@ -113,6 +114,9 @@ class HopSkipJumpAttack:
                 update = np.sign(gradf)
             else:
                 update = gradf
+
+            cos_details = self.capture_cosines(perturbed, original, gradf, a.true_label)
+            additional['cosine_details'].append(cos_details)
 
             logging.info('Binary Search back to the boundary')
             if self.stepsize_search == "geometric_progression":
@@ -174,6 +178,16 @@ class HopSkipJumpAttack:
             probs = self.model_interface.get_probs(point)
             screenshot[alpha] = probs.flatten()
         return screenshot
+
+    def capture_cosines(self, perturbed, original, gradf, true_label):
+        grad_st_line = perturbed - original
+        grad_true = self.model_interface.get_grads(perturbed[None], true_label)
+        _g_true = grad_true.flatten()
+        _g_line = grad_st_line.flatten()
+        _g_estm = gradf.flatten()
+        cos_true_vs_line = dot(_g_true, _g_line) / (norm(_g_true) * norm(_g_line))
+        cos_true_vs_estm = dot(_g_true, _g_estm) / (norm(_g_true) * norm(_g_estm))
+        return {'true_vs_line': abs(cos_true_vs_line), 'true_vs_estm': abs(cos_true_vs_estm)}
 
     def initialize_starting_point(self, a):
         success = 0

plot_radius_exp.py
@@ -4,19 +4,19 @@ import matplotlib.pylab as plt
 from model_factory import get_model
 
 NUM_ITERATIONS = 30
-NUM_IMAGES = 3
+NUM_IMAGES = 1000
 
 FLIP_PROB = 0.2
 
 model = get_model(key='mnist_noman', dataset='mnist')
 
 G = np.round(10 ** np.linspace(0, -2, num=9), 2)
-
+G = list(G) + [0.00127, 10]
 
 def read_dumps():
     raws = []
     for gamma in G:
-        folder = 'det_30_g{}'.format(gamma)
+        folder = 'det_30_1000_g{}'.format(gamma)
         filepath = 'adv/{}/raw_data.pkl'.format(folder)
         raws.append(pickle.load(open(filepath, 'rb')))
     return raws
@@ -26,13 +26,14 @@ raws = read_dumps()
 
 fig, ax1 = plt.subplots(figsize=(7, 7))
 ax1.set_xlabel('Iterations of the Attack')
-ax1.set_ylabel('L2 Distance')
-ax2 = ax1.twinx()
-ax2.set_ylabel('Proportion of Non-adversarial samples')
+ax1.set_ylabel('Median L2 Distance')
+ax1.set_yscale('log')
+# ax2 = ax1.twinx()
+# ax2.set_ylabel('Proportion of Non-adversarial samples')
 
 
 for i, raw in enumerate(raws):
-    adv_count = np.zeros(NUM_ITERATIONS, )
+    # adv_count = np.zeros(NUM_ITERATIONS, )
     medians = []
     for iteration in range(NUM_ITERATIONS):
         distances = []
@@ -40,19 +41,21 @@ for i, raw in enumerate(raws):
             if 'iterations' not in raw[image]:
                 continue
             distance = raw[image]['iterations'][iteration]['distance']
-            adversarial = raw[image]['iterations'][iteration]['perturbed']
-            probs = model.get_probs([adversarial])
+            # adversarial = raw[image]['iterations'][iteration]['perturbed']
+            # probs = model.get_probs([adversarial])
             label = raw[image]['true_label']
-            adv_prob = np.max(probs[0][np.arange(10) != label])
-            if np.argmax(probs[0]) != label:
-                adv_count[iteration] += 1
+            # adv_prob = np.max(probs[0][np.arange(10) != label])
+            # if np.argmax(probs[0]) != label:
+            #     adv_count[iteration] += 1
             distances.append(distance)
         medians.append(np.median(distances))
-    ax1.plot(medians, label='g = {}'.format(G[i]))
-    ax2.plot(1 - adv_count / NUM_IMAGES, '--', label='g = {}'.format(G[i]))
+    ax1.plot(range(1, 31), medians, label='g = {}'.format(G[i]), linewidth=0.9)
+    # ax2.plot(1 - adv_count / NUM_IMAGES, '--', label='g = {}'.format(G[i]))
 
 ax1.grid()
-plt.title('theta = g / sqrt(d), delta = theta * sqrt(d) * dist')
+plt.title('Radius Size Experiment with Deterministic Model (1000 images)'
+          '\ntheta = g / sqrt(d)'
+          '\ndelta = theta * sqrt(d) * L2_dist(t)')
 ax1.legend()
-plt.savefig('adv/radius_exp.png')
+plt.savefig('adv/radius_exp_1000.pdf')
 pass