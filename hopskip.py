f3c381455c55e3c962da149b553277e708309209
Noman Sheikh <nsheikh@student.ethz.ch>
2020-10-15 13:44:49 +0200

    hop skip with our delta changed to 1.5

--- hopskip.py
+++ hopskip.py
@@ -167,7 +167,7 @@ class HopSkipJumpRepeatedWithPSJDelta(HopSkipJump):
 
     def __init__(self, model_interface, data_shape, device=None, params: DefaultParams = None):
         super().__init__(model_interface, data_shape, device, params)
-        self.theta_det = 1. / self.grid_size
+        self.theta_det = 1.5 / self.grid_size
 
     def gradient_approximation_step(self, perturbed, num_evals_det, delta, dist_post_update, estimates, page):
         delta = dist_post_update * math.sqrt(self.d) / self.grid_size
