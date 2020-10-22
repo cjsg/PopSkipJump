254bfa9ac5f888846a34c1b7374f0693a94839b1
Noman Sheikh <nsheikh@student.ethz.ch>
2020-10-22 12:53:52 +0200

    dummy commit

--- app.py
+++ app.py
@@ -106,6 +106,7 @@ def merge_params(params: DefaultParams, args):
     return params
 
 
+
 def get_experiment_name(args, params):
     if params.experiment_name is not None:
         return params.experiment_name
