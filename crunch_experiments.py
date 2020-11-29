0d9b17139d3ef05893d6274be9f3a7f6316e74f7
Noman Sheikh <nsheikh@student.ethz.ch>
2020-11-29 20:06:06 +0100

    support for dropout noise and cropping noise

--- crunch_experiments.py
+++ crunch_experiments.py
@@ -12,17 +12,20 @@ dataset = sys.argv[2]
 flip_prob = float(exp_name.split('_')[-3])
 noise = exp_name.split('_')[-5]
 beta = float(exp_name.split('_')[-6])
-smoothing_noise = float(exp_name.split('_')[-8])
 device = get_device()
 NUM_ITERATIONS = 32
 NUM_IMAGES = int(exp_name.split('_')[-1])
+# NUM_IMAGES = 1
 eps = list(range(1, 6))
 if dataset == 'cifar10':
     d = 32*32*3
     model = get_model(key='cifar10', dataset=dataset, beta=beta)
+    model_noisy = get_model(key='cifar10', dataset=dataset, noise=noise)
 else:
     d = 28*28
     model = get_model(key='mnist_noman', dataset=dataset, beta=beta)
+    model_noisy = get_model(key='mnist_noman', dataset=dataset, noise=noise,
+                            smoothing_noise=0.01, crop_size=26, drop_rate=0.5)
 theta_det = 1 / (d * math.sqrt(d))
 
 
@@ -34,14 +37,30 @@ def read_dump(path):
 
 raw = read_dump(exp_name)
 model.model = model.model.to(device)
+model_noisy.model = model_noisy.model.to(device)
 
 
-def search_boundary(x_star, x_t, theta_det, true_label):
+def smoothing_output(x, true_label, samples=50):
+    dim = [samples] + [1] * x.dim()
+    x = x.unsqueeze(dim=0).repeat(*(dim))
+    pred = model_noisy.ask_model(x)
+    correct_pred = torch.sum(pred == true_label).float()
+    p = correct_pred / samples
+    if p >= 0.5:
+        return true_label
+    else:
+        return (true_label + 1) % 10
+
+
+def search_boundary(x_star, x_t, theta_det, true_label, smoothing=False):
     high, low = 1, 0
     while high - low > theta_det:
         mid = (high + low) / 2.0
         x_mid = (1 - mid) * x_star + mid * x_t
-        pred = torch.argmax(model.get_probs(x_mid[None])[0])
+        if smoothing:
+            pred = smoothing_output(x_mid, true_label)
+        else:
+            pred = torch.argmax(model.get_probs(x_mid[None])[0])
         if pred == true_label:
             low = mid
         else:
@@ -50,23 +69,30 @@ def search_boundary(x_star, x_t, theta_det, true_label):
     return out
 
 
-def project(x_star, x_t, label, theta_det):
-    probs = model.get_probs(x_t[None])
-    if torch.argmax(probs[0]) == label:
+def project(x_star, x_t, label, theta_det, smoothing=False):
+    if smoothing:
+        pred = smoothing_output(x_t, label)
+    else:
+        pred = torch.argmax(model.get_probs(x_t[None])[0])
+    if pred == label:
         c = 0.25
         while True:
             x_tt = x_t + c * (x_t - x_star) / torch.norm(x_t - x_star)
-            pred = torch.argmax(model.get_probs(x_tt[None])[0])
+            if smoothing:
+                pred = smoothing_output(x_tt, label)
+            else:
+                pred = torch.argmax(model.get_probs(x_tt[None])[0])
             if pred != label:
-                x_tt = search_boundary(x_t, x_tt, theta_det, label)
+                x_tt = search_boundary(x_t, x_tt, theta_det, label, smoothing)
                 break
             c += c
     else:
-        x_tt = search_boundary(x_star, x_t, theta_det, label)
+        x_tt = search_boundary(x_star, x_t, theta_det, label, smoothing)
     return x_tt
 
 
 D = torch.zeros(size=(NUM_ITERATIONS + 1, NUM_IMAGES), device=device)
+D_SMOOTH = torch.zeros_like(D, device=device)
 D_OUT = torch.zeros_like(D, device=device)
 MC = torch.zeros_like(D, device=device)
 AA = torch.zeros(size=(len(eps), NUM_ITERATIONS + 1, NUM_IMAGES), device=device)
@@ -79,7 +105,9 @@ for iteration in tqdm(range(NUM_ITERATIONS)):
         if iteration == 0:
             x_0 = diary.initial_projection
             x_00 = project(x_star, x_0, label, theta_det)
+            x_00_smooth = project(x_star, x_0, label, theta_det, smoothing=True)
             D[0, image] = torch.norm(x_star - x_00) ** 2 / d / 1 ** 2
+            D_SMOOTH[0, image] = torch.norm(x_star - x_00_smooth) ** 2 / d / 1 ** 2
             D_OUT[0, image] = -1
             MC[0, image] = diary.calls_initial_bin_search
 
@@ -87,8 +115,10 @@ for iteration in tqdm(range(NUM_ITERATIONS)):
         calls = page.calls.bin_search
         x_t = page.bin_search
         x_tt = project(x_star, x_t, label, theta_det)
+        x_tt_smooth = project(x_star, x_t, label, theta_det, smoothing=True)
 
         D[iteration + 1, image] = torch.norm(x_star - x_tt) ** 2 / d / 1 ** 2
+        D_SMOOTH[iteration + 1, image] = torch.norm(x_star - x_tt_smooth) ** 2 / d / 1 ** 2
         if exp_name.startswith('psj'):
             D_OUT[iteration + 1, image] = D[iteration, image]
         else:
@@ -110,8 +140,9 @@ for iteration in tqdm(range(NUM_ITERATIONS)):
 
 dump = {
     'border_distance': D,
+    'border_distance_smooth': D_SMOOTH,
     'attack_out_distance': D_OUT,
     'model_calls': MC,
     'adv_acc': AA,
 }
-torch.save(dump, open(f'{OUT_DIR}/{exp_name}/crunched.pkl', 'wb'))
+torch.save(dump, open(f'{OUT_DIR}/{exp_name}/crunched_new.pkl', 'wb'))
