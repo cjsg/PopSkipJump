d08a8fd1dc2fdc1c5ded385dcdc852342b852cd2
Noman Sheikh <nsheikh@student.ethz.ch>
2021-01-24 23:10:12 +0100

    [M] In crunching, stop binary search if image does not change anymore

--- crunch_experiments.py
+++ crunch_experiments.py
@@ -102,6 +102,8 @@ def project(x_star, x_t, label, theta_det, smoothing=False):
         while True:
             x_tt = x_t + c * (x_t - x_star) / torch.norm(x_t - x_star)
             x_tt = torch.clamp(x_tt, 0, 1)
+            if torch.all(x_tt == 1) or torch.all(x_tt == 0):
+                break
             if smoothing:
                 pred = smoothing_output(x_tt, label)
             else:
