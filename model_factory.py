ca127fe7c35f4977bdf61007012f4ebd2a78dcfd
Noman Sheikh <nsheikh@student.ethz.ch>
2021-02-01 15:51:07 +0100

    [A] True gradient input batch shipped to gpu

--- model_factory.py
+++ model_factory.py
@@ -86,7 +86,7 @@ class Model:
         # TODO: this line will not work for noisy model.
         # wrong_labels = self.ask_model(images)
         images = images.unsqueeze(1).type(torch.float32)
-        t_images = torch.tensor(images, requires_grad=True)
+        t_images = torch.tensor(images, requires_grad=True, device=self.device)
         t_outs = self.model(t_images)
         grad = torch.zeros(t_images.shape)
         for i in range(len(images)):
